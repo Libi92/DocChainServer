71cbd8346a5f126ee8823e5fb0e610457ef8ec57
Libin Babu <libinbabup@hotmail.com>
2019-05-06 05:06:42 +0530

    certificate enrolling

--- EduNet.py
+++ EduNet.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import flask
 from flask import Flask
 from flask_cors import CORS
@@ -55,10 +57,11 @@ def register():
     response = {'status': 200}
     if user:
         response['valid'] = True
+        response['user'] = user.inserted_id
     else:
         response['valid'] = False
 
-    return flask.jsonify(response)
+    return JSONEncoder().encode(response)
 
 
 @app.route('/student/create', methods=['POST'])
@@ -99,10 +102,11 @@ def create_student():
 
     if student:
         response['status'] = True
+        response['student'] = user.inserted_id
     else:
         response['status'] = False
 
-    return flask.jsonify(response)
+    return JSONEncoder().encode(response)
 
 
 @app.route('/student/get', methods=['POST'])
@@ -121,6 +125,74 @@ def get_student():
     response = {'status': 200, 'data': students_data}
 
     return JSONEncoder().encode(response)
+
+
+@app.route('/student/enroll', methods=['POST'])
+def enroll_student():
+    today = datetime.today()
+    current_year = today.year
+
+    req = flask.request.json
+    user = req['user']
+    degree = req['degree']
+    university = req['university']
+
+    client = MongoClient()
+    db = client.edunet
+    experiences = db.Experience
+    exp_object = {
+        'fromYear': current_year,
+        'toYear': current_year,
+        'company': university
+    }
+    experience = experiences.insert_one(exp_object)
+    experience_id = experience.inserted_id
+    exp_object['experienceId'] = experience_id
+    exp_object['$class'] = 'com.app.edunet.Experience'
+
+    cert_object = {
+        "degree": degree,
+        "year": current_year,
+        "active": True,
+        "certifiedUser": user,
+        "university": university,
+        "experience": [
+            experience_id
+        ]
+    }
+
+    certificates = db.Certificate
+    certificate = certificates.insert_one(cert_object)
+    certificate_id = certificate.inserted_id
+    cert_object['$class'] = 'com.app.edunet.Certificate'
+    cert_object['certificateId'] = certificate_id
+
+    del exp_object['_id']
+    del cert_object['_id']
+    response = {'status': 200, 'experience': exp_object, 'certificate': cert_object}
+    return JSONEncoder().encode(response)
+
+
+@app.route('/student/enroll/get', methods=['POST'])
+def get_enrolled_students():
+    req = flask.request.json
+    university = req['university']
+
+    client = MongoClient()
+    db = client.edunet
+    certificates = db.Certificate
+    students = db.Student
+    users = db.User
+
+    student_list = []
+    for certificate in certificates.find({'university': university}):
+        user = certificate['certifiedUser']
+        student = students.find_one({'userId': user})
+        if student:
+            student['user'] = users.find_one({'_id': user})
+            student_list.append(student)
+
+    return JSONEncoder().encode(student_list)
 
 
 if __name__ == '__main__':
